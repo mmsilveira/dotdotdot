a42d2fc3b0f0939ef284ead88599b3b9b59eb5fb
msilveira <msilveira.ti@gmail.com>
2019-03-02 13:27:28 -0300

    Organize repository

---
+++ 300-software-manual-installation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Moises Silveira
+##################################################################################################################
+
+# software from AUR (Arch User Repositories)
+# https://aur.archlinux.org/packages/
+
+
+echo "Installing category Accessories"
+
+
+echo "Installing category Development"
+sh manual/install-kubectl.sh
+
+
+echo "Installing category Education"
+
+
+echo "Installing category Games"
+
+
+echo "Installing category Graphics"
+
+
+echo "Installing category Internet"
+
+
+echo "Installing category Multimedia"
+
+
+echo "Installing category Office"
+
+
+echo "Installing category Other"
+
+
+echo "Installing category System"
+
+
+# these come always last
+
+
+echo "################################################################"
+echo "####        Software from AUR Repository installed        ######"
+echo "################################################################"
+
