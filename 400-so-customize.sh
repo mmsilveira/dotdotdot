58f2da6c769e38ed2b82d090ce038effeedd4f4b
Moises Silveira <msilveira.ti@gmail.com>
2019-10-29 08:41:39 -0300

    Configure group docker

--- 400-so-customize.sh
+++ 400-so-customize.sh
@@ -16,7 +16,13 @@ ln -sf $PWD/custom/terminator ~/.config/terminator/config
 
 echo "> System config"
 echo ">> Configure ssh dir"
-ln -sf /run/user/1000/keybase/kbfs/private/msilveira/ssh ~/.ssh
+if [ ! -e $HOME/.ssh ]; then
+    ln -s /run/user/1000/keybase/kbfs/private/msilveira/ssh $HOME/.ssh
+fi
+
+echo ">> Configure Docker"
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 echo "################################################################"
 echo "####                 Customization applied                ######"
