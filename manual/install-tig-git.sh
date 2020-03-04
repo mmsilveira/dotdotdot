6a003c2315fbc4b3068588b9a38c3a81713c906f
Moises Silveira <msilveira.ti@gmail.com>
2020-03-04 09:13:29 -0300

    Fix problem that don't remove tar package after instalation

--- manual/install-tig-git.sh
+++ manual/install-tig-git.sh
@@ -7,10 +7,12 @@ set -e
 
 #----------------------------------------------------------------------------------
 
+echo '> Installing TIG git tool'
+
 curl -L https://github.com/jonas/tig/releases/download/tig-2.4.1/tig-2.4.1.tar.gz > tig.tar.gz
 tar -xzvf tig.tar.gz
 cd tig-2.4.1
 make
 make install
-rm -rf tig-2.4.1
-rm -f tig.tar.gz
+rm -rf ../tig-2.4.1
+rm -f ../tig.tar.gz
