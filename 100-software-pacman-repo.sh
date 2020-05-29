4d32e1dc4557ac704783e7e4239ca7f9bc6c0900
Moises Silveira <msilveira.ti@gmail.com>
2020-05-29 10:09:20 -0300

    Comment firefox instalation

--- 100-software-pacman-repo.sh
+++ 100-software-pacman-repo.sh
@@ -30,7 +30,7 @@ sudo pacman -S --noconfirm --needed gnome-font-viewer # GNOME Fonts shows you th
 #sudo pacman -S --noconfirm --needed
 
 echo "Installing category Internet"
-sudo pacman -S --noconfirm --needed firefox # Browser
+#sudo pacman -S --noconfirm --needed firefox # Browser
 #sudo pacman -S --noconfirm --needed brave   # Browser
 #sudo pacman -S --noconfirm --needed
 
