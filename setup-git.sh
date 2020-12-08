79d4b799bacaac28ec4f7d6606501886629bdfd6
Moises Silveira <msilveira.ti@gmail.com>
2020-12-08 14:22:01 -0300

    Add new alias to git for list last worked branchs

--- setup-git.sh
+++ setup-git.sh
@@ -15,6 +15,7 @@ git config --global credential.helper 'cache --timeout=25000'
 git config --global push.default simple
 git config --global pull.rebase true
 git config --global alias.pushf "push --force-with-lease"
+git config --global alias.wip "for-each-ref --sort='authordate:iso8601' --format=' %(color:green)%(authordate:relative)%09%(color:white)%(refname:short)' refs/heads"
 sudo git config --system core.editor nvim
 
 echo "################################################################"
